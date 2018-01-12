1169f32d293fd5abe0a5bbd2408604bab97c0679
Simon Zeng <szeng@Simons-MacBook-Pro.local>
2018-01-12 07:07:47 -0700

    form

--- index.php
+++ index.php
@@ -2,20 +2,16 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: TangledDemo'; 
-    $to = 'demo@tangledindesign.com'; 
-    $subject = 'Hello';
-    $human = $_POST['human'];
+    $from = 'From: Your Website'; 
+    $to = 'simonzeng9@gmail.com'; 
+    $subject = 'Feedback';
 			
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 				
-    if ($_POST['submit'] && $human == '4') {				 
+    if ($_POST['submit']) {				 
         if (mail ($to, $subject, $body, $from)) { 
 	    echo '<p>Your message has been sent!</p>';
 	} else { 
 	    echo '<p>Something went wrong, go back and try again!</p>'; 
 	} 
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
-    }
 ?>
